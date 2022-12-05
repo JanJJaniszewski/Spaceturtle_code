b0159ce8cc90912def469dae505f66299d8d90f3
Jan Janiszewski <pole19901990@gmail.com>
2022-12-05 22:53:59 +0100

    removed secrets.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import pyodbc
-
-driver= '{ODBC Driver 13 for SQL Server}'
-
-with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-        row = cursor.fetchone()
-        while row:
-            print (str(row[0]) + " " + str(row[1]))
-            row = cursor.fetchone()
+import pandas as pd
+import Config.config_main as cm
+import Config.secrets as ss
+import Functions.Utils as ut
