5c8ec4d3431fb72d5c059330411335ed2dd47030
Jan Janiszewski <pole19901990@gmail.com>
2022-12-05 22:43:27 +0100

    Initial commit

---
+++ Config/secrets.py
@@ -0,0 +1,4 @@
+server = 'spaceturtle.database.windows.net'
+database = 'spaceturtle'
+username = 'spaceturtle'
+password = 'abKhzQip1|-l'
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import pyodbc
-server = 'spaceturtle.database.windows.net'
-database = 'spaceturtle'
-username = 'spaceturtle'
-password = 'abKhzQip1|-l'
+
 driver= '{ODBC Driver 13 for SQL Server}'
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
